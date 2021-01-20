db650fafcdc5b6098bdae3daa4e79002d1f333c4
PraneethS <psomiset@calpoly.edu>
2021-01-19 16:25:04 -0800

    added thank you

--- contact.php
+++ contact.php
@@ -27,7 +27,7 @@ $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $to \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
-header('Location: index.html');
+header('Location: thank-you.html');
 
 
 // Function to validate against any email injection attempts
